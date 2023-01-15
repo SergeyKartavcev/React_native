a5222ebfccef6df69aa10214160b21d286d71497
SergeyKartavcev <102188021+SergeyKartavcev@users.noreply.github.com>
2023-01-15 18:18:40 +0200

    add

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! </Text>
-      <Text> Hello world! Howe are you </Text>
+      <Text> Hello world! Howe are you. Ctoto ne tack , kryvaya ssilka </Text>
       <StatusBar style="auto" />
     </View>
   );
